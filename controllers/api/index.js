887c0d36a38b44915451c59c79f5f731ba4ac8e3
Hailey Francis <n01402670@unf.edu>
2023-02-22 21:23:54 -0500

    added commentRoutes to index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require("./userRoutes");
 const postRoutes = require("./postRoutes");
-//commentRoutes
+const commentRoutes = require("./commentRoutes");
 
 router.use("/users", userRoutes);
-router.use("posts", postRoutes);
-//commentRoutes
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
