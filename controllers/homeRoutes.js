3122c31281e55c1b74bb0ecf4f1c60cf567b748b
Hailey Francis <n01402670@unf.edu>
2023-02-24 15:55:26 -0500

    can execute server command, sessions are being created, however handlebars not displaying

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { Project, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+/**
+ * Route to display projects on homepage
+ */
+router.get("/", async (req, res) => {
+    try{
+        //get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        //serialize data so the template can read it
+        const posts = postData.map((post) => post.get({plain: true}));
+
+        //pass serialized data and session flag into template
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
